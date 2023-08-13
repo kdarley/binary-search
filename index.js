b773302132d0c73d01d20a6e2fb945dd1bb59d73
KEVIN DARLEY <39971882+kdarley@users.noreply.github.com>
2023-08-13 16:56:56 -0500

    can insert and delete nodes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("hello world")
+// console.log("hello world")
 
 const Node = (data = null, left = null, right = null) => {
     this.data = data
@@ -13,7 +13,6 @@ const Node = (data = null, left = null, right = null) => {
 }
 
 const Tree = (array) => {
-
     function cleanArray(array) {
         //if array is empty, return
         if (array===null || array.length===0){
@@ -44,56 +43,121 @@ const Tree = (array) => {
     };
 
     function buildTree(array){
-        // console.log(array)
         // base case
-        //if array is empty, return
-        if (array === undefined, array.length===0){
+        if (array === null || array.length===0){
             return null
         } else {
-            // console.log(array)
             // find center index  of array
             const rootIndex = Math.floor(array.length/2) // floor because js ar is indexed 0
-            // define node
-            // console.log(rootIndex)
-            const leftArray = array.slice(0, rootIndex)
+            // split the array 
+            const leftArray = array.slice(0, rootIndex) // array does not include index
             const rightArray = array.slice(rootIndex+1)
-            // console.log("left", leftArray)
-            // console.log("right", rightArray)
+
+            // create the node recursively
             const node = Node(array[rootIndex], buildTree(leftArray), buildTree(rightArray))
-            // node.left = buildTree(array.slice(0, rootIndex))
-            // node.right = buildTree(array.slice(-rootIndex))
 
             return node
-
-
-
-            // console.log(arr.slice(0, rootIndex))
-            // console.log(arr.slice(-rootIndex))
-
-
-
-            // let currentNode = Node(arr.at(rootIndex))
-            // console.log(currentNode)
-            // if (root===null){
-            //     root = currentNode
-
-            //     currentNode.left = buildTree(arr = arr.slice(0, rootIndex))
-            //     currentNode.right = buildTree(arr = arr.slice(-rootIndex))
-            // } else {
-            //     currentNode.left = buildTree(arr = arr.slice(0, rootIndex))
-            //     currentNode.right = buildTree(arr = arr.slice(-rootIndex))
-                
-            // }
         }     
 
     }
 
+    const insert = (value, currentNode = root) => {
+        let nodeValue = currentNode.data
+        if (value < nodeValue){
+            if (currentNode.left === null) {
+                currentNode.left = Node(value)
+            } else {
+                insert(value, currentNode.left)
+            }
+        } else if (value > nodeValue){
+            if (currentNode.right === null){
+                currentNode.right = Node(value)
+            } else {
+                insert(value, currentNode.right)
+            }
+        } else { // when values are equal
+            return
+        }
+    }
+
+    // function findNode(value, currentNode = root){
+    //     if (currentNode === null){
+    //         return 
+    //     }
+    //     let nodeValue = currentNode.data
+
+    //     if (value === nodeValue){
+    //         return currentNode
+    //     } 
+    //     if (value < nodeValue){
+    //         if (currentNode.left !== null){
+    //             currentNode = findNode(value, currentNode.left)
+    //             return currentNode
+    //         } 
+    //     } else {
+    //         if (currentNode.right !== null){
+    //             currentNode = findNode(value, currentNode.right)
+    //             return currentNode
+    //         } 
+    //     }
+    // }
+
+    const minValue = (node) => {
+        let minValue = node.data;
+        while (node.left) {
+            minValue = node.left.data;
+            node = node.left;
+        }
+        return minValue;
+    };
+
+    const deleteNode = (value, currentNode = root) => {
+        if (currentNode === null){
+            return null; 
+        }
+        // if node has one child, replace node with child
+        // if node has two children, find minimum within sub tree and replace with child
+
+        // find the node
+        if (value < currentNode.data){
+            currentNode.left = deleteNode(value, currentNode.left)
+        } else if (value > currentNode.data) {
+            currentNode.right = deleteNode(value, currentNode.right)
+        } else { // if the value is equal to node data
+            // if either or one of children are empty
+            if (currentNode.left === null){
+                return currentNode.right // in this case the right node is either null or the node to be set to
+            } else if (currentNode.right === null) {
+                return currentNode.left // in this case we know the left node is not null and just need to reassign the tree
+            }
+
+            //if both children contain data
+            // assign the data of the current node to the min of the right node
+            currentNode.data = minValue(currentNode.right)
+
+            // delete the min node in the sub tree
+            currentNode.right = deleteNode(currentNode.data, currentNode.right)
+        }
+
+        return currentNode
+    }
+
+
+
     return {
         prettyPrint,
-        buildTree
+        insert, 
+        deleteNode
     }
 }
 
-let t = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324,6 ,3 ,2, 19, 10])
+let t = Tree([1, 7, 4, 23, 8, 9, 4, 3, 7, 9, 67, 6345, 324,6 ,3 ,2, 19, 10])
 // let t = Tree([])
+t.prettyPrint()
+t.deleteNode(8)
+t.deleteNode(7)
+t.prettyPrint()
+t.deleteNode(9)
+t.deleteNode(1)
+t.deleteNode(2)
 t.prettyPrint()
